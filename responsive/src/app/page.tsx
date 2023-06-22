3e5e02e8d03361d98d481050476c67e4c67da7d8
willianchu <willianchu@hotmail.com>
2023-06-22 13:37:36 -0700

    set html tags

--- responsive/src/app/page.tsx
+++ responsive/src/app/page.tsx
@@ -3,13 +3,47 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-        priority
-       />
+      <div className="header">
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          width={100}
+          height={24}
+          priority
+        />
+      <div>
+        <h1> Greetings </h1>
+        <h2> Description </h2>
+      </div>
+      <div className="experience">
+        <h3> Experience </h3>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <div className="experience-time">
+          <p> 2019 - 2020 </p>
+        </div>
+        <div className="infos">
+          <h3> Infos </h3>
+          <div className="languages-info">
+            <span> ⚽Portuguese (Brazil) - Native </span>  
+            <span> 🏈English - Intermediate</span>
+            <span> 💃Spanish - Basic </span>
+            <span> 🥐French - Basic </span>
+          </div>
+          <h3> Education </h3>
+          <div className="educational-info">
+            <span> 🎓 </span>
+            <span> School </span>
+          </div>
+        </div>
+        <div className="buttons">
+          <div className="social">
+
+          </div>
+          <button> Call me! </button>
+        </div>
+
+      </div>
+      </div>
     </main>
   )
 }
