8ecc54427ccd07b7bf527667c60117b7526622e8
willianchu <willianchu@hotmail.com>
2023-06-26 13:47:43 -0700

    social buttons

--- responsive/src/app/components/social-btns/social-btns.tsx
+++ responsive/src/app/components/social-btns/social-btns.tsx
@@ -1,9 +1,17 @@
+import { GitHubIcon } from "../icons/github-icon";
+import { LinkedinIcon } from "../icons/linkedin-icon";
+
+import './social-btns.scss';
+
 export function SocialBtns() {
   return (
     <div className="social">
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
+      <a href="">
+        <LinkedinIcon/>
+      </a>
+      <a href="">
+        <GitHubIcon/>
+      </a>
     </div>
   );
 }
--- responsive/src/app/page.tsx
+++ responsive/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Experience } from "./components/experience/experience";
 import { Header } from "./components/header/header";
 import { Info } from "./components/information/information";
+import { SocialBtns } from "./components/social-btns/social-btns";
 import './styles/home.scss'
 
 export default function Home() {
@@ -10,9 +11,7 @@ export default function Home() {
       <Experience/>
       <Info/>
         <div className="buttons">
-          <div className="social">
-
-          </div>
+          <SocialBtns/>
           <button className="btn-primary"> Call me! 🤙 </button>
         </div>
 
